471ba683eade5d68104c752ba7b4d03d912e452d
Nischal Lama <lamanischal9999@gmail.com>
2026-04-01 11:48:08 +0000

    added dashboard

---
+++ dashboard.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "you are at dashboard"
+
--- login.sh
+++ login.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+
+#addding new feature
+
+
 echo "some change in login"
 
-#addding new feature 
+df -h
+hostname
+date
